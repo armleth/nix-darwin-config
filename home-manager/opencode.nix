d04b0edb301e3cdd00d3cd27baf70ff96e259fb1
Armand Thibaudon <armand.thibaudon@datadoghq.com>
2026-02-13 12:14:25 +0100

    feat: opencode config addition

---
+++ home-manager/opencode.nix
@@ -0,0 +1,31 @@
+{ config, pkgs, ... }:
+
+{
+    programs.opencode.settings = {
+        "$schema" = "https://opencode.ai/config.json";
+        "disabled_providers" = [ "opencode" ];
+        "share" = "disabled";
+        "permission" = {
+            "edit" = "ask";
+            "webfetch" = "ask";
+            "doom_loop" = "ask";
+            "external_directory" = "ask";
+            "bash" = {
+                "*" = "ask";
+                "pwd" = "allow";
+                "git status" = "allow";
+                "git log *" = "allow";
+                "git show *" = "allow";
+                "git diff *" = "allow";
+                "git blame *" = "allow";
+                "gh pr view *" = "allow";
+                "grep *" = "allow";
+                "rg *" = "allow";
+                "ls *" = "allow";
+                "tail *" = "allow";
+                "head *" = "allow";
+                "cat *" = "allow";
+            };
+        };
+    };
+}
